b8e551414e8a27f2440d3f359215fd14c7b97173
Adeel <adeeltahir6a@gmail.com>
2026-03-20 22:53:10 +0500

    Fix typo in BeautifulSoup import and usage in script.py

--- script.py
+++ script.py
@@ -3,13 +3,13 @@ from rss_feed import feed_sources
 from parser import feed_parser
 from website_generator import parsed_content_to_html, save_html_to_file
 from html_to_zim import site_to_zim
-from bs4 import BeautifulSoap
+from bs4 import BeautifulSoup
 def main():
     for feed_source in feed_sources:
         parsed_content=feed_parser(feed_source)
         main_page,episodes=parsed_content_to_html(parsed_content)
         save_html_to_file(main_page,"index.html")
-        soup=BeautifulSoap(episodes,"html.parser")
+        soup=BeautifulSoup(episodes,"html.parser")
         episodes=soup.find_all("div")
         for i,ep in enumerate(episodes):
             save_html_to_file(ep,f"episode_{i}.html")
