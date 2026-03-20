e1cb7fdce98b90515f45bf5d46526bb00ca5a77b
Adeel <adeeltahir6a@gmail.com>
2026-03-20 23:11:44 +0500

    Refactor website_generator.py for improved HTML generation and update .gitignore to include static_website

--- website_generator.py
+++ website_generator.py
@@ -1,21 +1,26 @@
+import os
 def parsed_content_to_html(parsed_content):
-
     main_page=""
     episode=""
     episodes_pages=""
-    episodes=parsed_content.episodes
+    episodes=parsed_content.entries
     
-    main_page=f"<h1>{parsed_content.title}</h1>"
-    main_page+="<ul>"
-    for i,ep in episodes:
-     main_page+=f"<li><a href=./episode_{i}.html></li>"
+    main_page = f"<h1>{parsed_content.feed.title}</h1>\n<ul>\n"
+    for i,ep in enumerate(episodes):
+    # Audio file
+     if ep.enclosures:
+        audio_file = ep.enclosures[0].href
+     else:
+        audio_file = ep.link  
+     
+     main_page += f'<li><a href="./episode_{i}.html">{ep.title}</a></li>\n'
      episode= f"""
-        <div>
-        <h2>{ep['title']}</h2>
-        <p>{ep['description']}</p>
-        <audio controls>
-            <source src="../{ep['audio_file']}" type="audio/mpeg">
-        </audio>
+         <div>
+            <h2>{ep.title}</h2>
+            <p>{getattr(ep, 'description', '')}</p>
+            <audio controls>
+                <source src="{audio_file}" type="audio/mpeg">
+            </audio>
         </div>
         """
      episodes_pages+=episode
@@ -24,7 +29,11 @@ def parsed_content_to_html(parsed_content):
 
    
 
-def save_html_to_file(html,path):
-    directory="./static_website"
-    with open(f"./static_website/{path}","w") as f:
+def save_html_to_file(html, path):
+    directory = "./static_website"
+    # create directory if it doesn't exist
+    os.makedirs(directory, exist_ok=True)
+    with open(os.path.join(directory, path), "w", encoding="utf-8") as f:
         f.write(html)
+
+
